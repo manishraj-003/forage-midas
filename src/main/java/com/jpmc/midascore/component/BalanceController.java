977317d674c908b482a1e25b90125af01d118fcb
manishraj-003 <manishrajofficial777@gmail.com>
2025-11-21 19:43:28 +0530

    Implement BalanceController to retrieve user balance

---
+++ src/main/java/com/jpmc/midascore/component/BalanceController.java
@@ -0,0 +1,28 @@
+package com.jpmc.midascore.component;
+
+import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Balance;
+import com.jpmc.midascore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class BalanceController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/balance")
+    public Balance getBalance(@RequestParam Long userId) {
+        Optional<UserRecord> user = userRepository.findById(userId);
+        if (user.isPresent()) {
+            return new Balance(user.get().getBalance());
+        } else {
+            return new Balance(0.0f);
+        }
+    }
+}
